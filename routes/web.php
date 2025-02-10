296435af95ad76668d4187744faff928650769c6
IRAD JOSEPH SOPIO ARANTE <1170082@cc.usls.edu>
2025-02-10 09:14:29 +0800

    Hello World

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route:: get('/item', 'App\Http\Controllers\ItemController@index');
 Route:: get('/login', 'App\Http\Controllers\LoginController@index');
 
 Route:: get('/aboutme', 'App\Http\Controllers\AboutmeController@index');
+
+Route:: get('/hello', 'App\Http\Controllers\HelloController@index');
